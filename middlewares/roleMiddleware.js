3ad2e24ad72821c34542547e37f01e9bb783e3d2
Gaurav Wakhare <gwakhare2418@gmail.com>
2025-03-28 10:52:40 +0530

    access control achieved

---
+++ middlewares/roleMiddleware.js
@@ -0,0 +1,12 @@
+const authorizeRoles = (...allowedRoles) => {
+return (req, res, next) => {
+    
+    if(!allowedRoles.includes(req.user.role)){
+        return res.status(403).send("access denied")
+        
+    };
+    next()
+}
+}
+
+module.exports = authorizeRoles
