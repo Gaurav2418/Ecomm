502e39b34cfe13c95fc00957f190c71984093540
Gaurav Wakhare <gwakhare2418@gmail.com>
2025-03-28 15:53:15 +0530

    shopcotroller added

--- middlewares/verifyMiddleware.js
+++ middlewares/verifyMiddleware.js
@@ -1,16 +1,23 @@
 const jwt = require("jsonwebtoken")
 
 const verifyToken = async (req, res, next) => {
-    const authHeader = req.headers.authorization || req.headers.authorization
-    if(authHeader){
-        const token = authHeader.split(" ")[1]   
-        console.log(token)
-        const decode = jwt.verify(token, process.env.jwt_secret)
-        req.user = decode
-        console.log(decode)
-        next()
-    }else{
-        return res.status(403).send("Access Denied, No token found")
+    
+    try {
+        const authHeader = req.headers.authorization || req.headers.authorization
+        if(authHeader){
+            const token = authHeader.split(" ")[1]   
+            // console.log(token)
+            const decode = jwt.verify(token, process.env.jwt_secret)
+            req.user = decode
+            // console.log(decode)
+            next()
+        }else{
+            return res.status(403).send("Access Denied, No token found")
+        }
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send("Something went wrong. Please try again later.")
     }
 }
 
---
+++ src/controllers/shopController.js
@@ -0,0 +1,69 @@
+const shopkeeperModel = require("../models/shopkeeperModel")
+const allUserModel = require("../models/authModel")
+const saleModel = require("../models/saleModel")
+
+
+const paymentController = (req, res) => {
+
+}
+
+
+const saleController = async (req, res) => {
+const {brands, category} = req.body
+
+if(!brands && !category){
+    return res.status(400).send("To post a sale you must enter Brands or category")
+}
+
+const data = await saleModel({
+    userDetails:req.user.id,
+    brands,
+    category
+}).save()
+ // Find the newly created sale document
+console.log(data._id)
+ const populatedData = await saleModel.findById(data._id).populate('userDetails')
+
+
+return res.status(200).send({
+    message:"sale has been created with following data",
+    populatedData
+})
+}
+
+
+const getProfileController = async (req, res) => {
+console.log(req.user)
+const owner = await allUserModel.findOne({_id: req.user.id})                    // data from alluser collection (i.e. auth data name, email, role)
+const profData = await shopkeeperModel.findOne({email: req.user.email})         // data from shopowner collection
+
+// we can access here both alluser, shopowner collections from db
+console.log(profData)
+console.log(owner)
+return res.status(200).send(owner)
+}
+
+
+const createProfileController = async (req, res) => {
+    console.log(req)
+    // searching for user data in alluser model using id coming from req data 
+    const owner = await allUserModel.findOne({_id: req.user.id})
+    console.log(owner)
+
+    const shopkeeperDocumentId = owner._id 
+    req.user = shopkeeperDocumentId
+
+    const shoplocation = "nagar"
+    // Creating profile and saving in db
+    const data = await shopkeeperModel({
+        userDetails: owner._id,
+        shopLocation:shoplocation
+    }).save()
+    return res.status(200).send(data)
+    }
+    
+
+
+
+
+module.exports = { paymentController, saleController, getProfileController, createProfileController}
