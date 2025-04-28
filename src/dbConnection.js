18e19b2131144bb926d790910954aab656978089
Gaurav Wakhare <123626617+Gaurav2418@users.noreply.github.com>
2025-04-28 16:59:11 +0530

    Update dbConnection.js

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -3,8 +3,8 @@ const EcommDB ="EcommDb"
 const DB1 = "DB1"
 const connectDB = async () => {
     try{
-        await mongoose.connect(`${process.env.mongo_url_local}${EcommDB}`)
-        // await mongoose.connect(`${process.env.mongo_server}${DB1}`)
+        // await mongoose.connect(`${process.env.mongo_url_local}${EcommDB}`)
+        await mongoose.connect(`${process.env.mongo_server}${DB1}`)
 
         console.log(`connected to database ${mongoose.connection.host}`)
         
